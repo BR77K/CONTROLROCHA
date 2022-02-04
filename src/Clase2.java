fcdb22e970094b55527fa3a99e8f41bcc9da4c07
dam <dam@203P12>
2022-02-04 12:10:10 +0100

    Versión_BETA

---
+++ src/Clase2.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author dam
+ */
+public class Clase2 {
+    public void segundaParte (){
+        System.out.println("Segunda clase");
+        System.out.println("Segunda línea");
+    }
+}
